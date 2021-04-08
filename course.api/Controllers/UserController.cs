ed22e7a52bd296ef562c1c10fa1133fa8f768641
Mihainuan DeSá Silva <mihainuan@gmail.com>
2021-04-08 15:15:11 -0300

    Configuração com 'provider' JWT Implementações em 'Startup.cs'

--- course.api/Controllers/UserController.cs
+++ course.api/Controllers/UserController.cs
@@ -2,10 +2,14 @@
 using course.api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace course.api.Controllers
@@ -22,12 +26,35 @@ namespace course.api.Controllers
         [ValidacaoModelStateCustom]
         public IActionResult Logmein(LoginViewModelInput loginViewModelInput)
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
-            //}
+            var userViewModelOutput = new UserViewModelOutput()
+            {
+                CodeID = 1,
+                Login = "mihainuan",
+                Email = "mihainuan@gmail.com"
+            };
 
-            return Ok(loginViewModelInput);
+            var secret = Encoding.ASCII.GetBytes("MNaAlpB%&<!P@LOO$@!>777*H3lp?#");
+            var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userViewModelOutput.CodeID.ToString()),
+                    new Claim(ClaimTypes.Name, userViewModelOutput.Login.ToString()),
+                    new Claim(ClaimTypes.Email, userViewModelOutput.Email.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenGenerated = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokenGenerated);
+
+            return Ok(new
+            {
+                Token = token,
+                User = userViewModelOutput
+            });
         }
 
         [SwaggerResponse(statusCode: 200, description: "SUCESSO no Registro!", Type = typeof(LoginViewModelInput))]
---
+++ course.api/Models/UserViewModelOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace course.api.Models
+{
+    public class UserViewModelOutput
+    {
+        public int CodeID { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+    }
+}
