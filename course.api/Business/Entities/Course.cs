2e5686b87cd0a29ec93c8b50198389fe5677efb6
Mihainuan DeSá Silva <mihainuan@gmail.com>
2021-04-09 09:32:33 -0300

    Criação das Classes

---
+++ course.api/Business/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace course.api.Business.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
